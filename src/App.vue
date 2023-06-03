85f17d14c797f154010db3fc6a9cacf60a2bcfc6
Zak Dowsett <z.dowsett@outlook.com>
2023-06-03 23:07:53 +0100

    add development warning

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
 <template>
+  <v-banner elevation="5" style="z-index: 1000;" position="fixed" icon="fas fa-triangle-exclamation" color="yellow-accent-4">
+    <v-banner-text>
+      This site is currently under development. If you would like to know more about me feel free to 
+    <a href="mailto:z.dowsett@outlook.com">reach out</a> 
+    </v-banner-text>
+  </v-banner>
   <router-view />
 </template>
 
