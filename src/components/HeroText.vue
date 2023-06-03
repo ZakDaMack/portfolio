a8dcb722d71ba79a499e7d44cba7d33643dcd744
Zak Dowsett <z.dowsett@outlook.com>
2023-06-03 22:20:49 +0100

    initial

---
+++ src/components/HeroText.vue
@@ -0,0 +1,90 @@
+<template>
+    <div id="hero-text">
+        <div class="line">
+            <div class="word">Zak</div>
+            <div class="word">Dowsett</div>
+        </div>
+        <div class="line">
+            <div class="word">FullStack</div>
+            <div class="word">Dev</div>
+        </div>
+        <!-- internal links -->
+        <div class="line">
+            <div data-value="CV" class="word styled">CV</div>
+            <div data-value="PROJECTS" class="word styled">Projects</div>
+        </div>
+        <!-- links -->
+        <div class="line">
+            <a data-value="LINKEDIN" class="word styled" href="https://linkedin.com/in/zak-dowsett-4a7455131/" target="_blank">LinkedIn</a>
+            <a data-value="EMAIL" class="word styled" href="mailto:z.dowsett@outlook.com" target="_blank">Email</a>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let interval = null;
+
+onMounted(() => {
+  const els = document.querySelectorAll(".styled");
+  els.forEach(el => el.onmouseover = e => {
+    let iteration = 0;
+  
+    clearInterval(interval);
+    interval = setInterval(() => {
+        e.target.innerText = e.target.innerText
+        .split("")
+        .map((l, index) => {
+            if(index < iteration) {
+            return e.target.dataset.value[index];
+            }
+        
+            return letters[Math.floor(Math.random() * 26)]
+        })
+        .join("");
+        
+        if(iteration >= e.target.dataset.value.length){ 
+        clearInterval(interval);
+        }
+        
+        iteration += 1 / 3;
+    }, 30);
+  })
+
+  els.forEach(el => el.onmouseleave = e => {
+    e.target.innerText =  e.target.dataset.value[index];
+  })
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+#hero-text:has(.styled:hover) .word:not(.styled:hover) {
+  opacity: 0.3;
+}
+
+.word {
+  font-family: 'Rubik Mono One', sans-serif;
+  font-size: clamp(1rem, 5vw, 6rem);
+  color: white;
+  text-transform: uppercase;
+  line-height: 1em;
+  transition: opacity 300ms ease; 
+}
+.word:not(:last-child) {
+  margin-right: 1em;
+}
+
+.styled {
+  cursor: pointer;
+  text-decoration: none;
+}
+
+.line {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,100 @@
+<template>
+  <v-img cover height="100vh" src="https://images.unsplash.com/photo-1604223190546-a43e4c7f29d7" gradient="90deg, black 0%, transparent 60%">
+    <v-container class="h-screen restrict-w d-flex align-center justify-center justify-md-space-between">
+      
+      <hero-text></hero-text>
+      
+      <v-avatar size="200" class="d-none d-md-block">
+        <v-img
+          cover
+          src="https://images.unsplash.com/photo-1519575706483-221027bfbb31"
+          alt="Zak"
+        ></v-img>
+      </v-avatar>
+      
+    </v-container>
+  </v-img>
+
+  <!-- projects -->
+  <div class="grid">
+    <div class="title">Projects</div>
+
+    <v-img cover src="https://images.unsplash.com/photo-1580086319619-3ed498161c77">
+      <div class="center-grid fit-bg">
+        <h3 class="text-h1 pa-2 fit-logo">1FIT</h3>
+      </div>
+      
+    </v-img>
+    <v-img cover src="https://images.unsplash.com/photo-1481437156560-3205f6a55735">
+      <div class="center-grid bs-bg">
+      <h3 class="text-h1 pa-2 bs-logo">Blue Square</h3>
+      </div>
+    </v-img>
+  </div>
+
+  <one-fit />
+  <blue-square />
+</template>
+
+<script setup>
+import HeroText from "@/components/HeroText.vue";
+import OneFit from "@/components/projects/1Fit.vue"
+import BlueSquare from "@/components/projects/Bluesquare.vue"
+</script>
+
+<style lang="scss" scoped>
+.grid {
+  position: relative;
+  display: grid;
+  cursor: pointer;
+  // grid-template-columns: 1fr 1fr;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  .v-img {
+    aspect-ratio: 1;
+  }
+}
+
+.center-grid {
+  display: grid;
+  height: 100%;
+  h3 {
+    place-self: center;
+  }
+}
+
+.title {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  font-family: 'Rubik Mono One', sans-serif;
+  font-size: clamp(2rem, 3vw, 6rem);
+  color: white;
+  text-transform: uppercase;
+  line-height: 1em;
+  transition: opacity 300ms ease; 
+  z-index: 10;
+}
+
+.fit-logo {
+  background: linear-gradient(45deg, #1D212D, #2CF5B8 50%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.fit-bg {
+  background: radial-gradient(circle at bottom left, rgba(#2CF5B8,0.5), rgba(#1D212D,0.5) 50%);
+}
+
+.bs-logo {
+  background: linear-gradient(45deg, #182741, #0CA3E5 50%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.bs-bg {
+  background: radial-gradient(circle at bottom left, rgba(#0CA3E5,0.5), rgba(#1D212D,0.8) 50%);
+}
+
+</style>
