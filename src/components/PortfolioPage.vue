6f4d4fb31e52f352a5eac80331506f5d9fc1b568
Zak Dowsett <z.dowsett@outlook.com>
2023-10-09 04:36:32 +0100

    change max article width

--- src/components/PortfolioPage.vue
+++ src/components/PortfolioPage.vue
@@ -1,18 +1,20 @@
 <template>
     <v-container fluid :style="cssProps" class="bg-grad">
-        <div class="d-flex" :class="{'mb-8': !mobile}">
-            <v-btn
-                class="abs-btn pt-8"
-                prepend-icon="fas fa-chevron-left"
-                color="white" size="large"
-                to="/" variant="plain"
-            >{{ mobile ? '' : 'Back' }}</v-btn>
-            <v-spacer></v-spacer>
-            <h2 class="text-h1 logo text-right">{{ props.name }}</h2>
-        </div>
-        <slot></slot>
-        <div class="text-center">
-            <v-btn size="x-large" class="rounded-xl" :href="props.siteUrl" target="_blank">Visit site</v-btn>
+        <div style="max-width: 100em;" class="mx-auto">
+            <div class="d-flex" :class="{'mb-8': !mobile}">
+                <v-btn
+                    class="abs-btn pt-8"
+                    prepend-icon="fas fa-chevron-left"
+                    color="white" size="large"
+                    to="/" variant="plain"
+                >{{ mobile ? '' : 'Back' }}</v-btn>
+                <v-spacer></v-spacer>
+                <h2 class="text-h1 logo text-right">{{ props.name }}</h2>
+            </div>
+            <slot></slot>
+            <div class="text-center">
+                <v-btn size="x-large" class="rounded-xl" :href="props.siteUrl" target="_blank">Visit site</v-btn>
+            </div>
         </div>
     </v-container>
 </template>
